d438bf28992815159e20a633750214828cfb3e34
nehoraitubul <nehorai0258@gmail.com>
2025-05-19 21:21:31 +0300

    REGULAR

---
+++ src/main/java/Semester2/OOP1/Lesson5/Seker/Person.java
@@ -0,0 +1,12 @@
+package Semester2.OOP1.Lesson5.Seker;
+
+import com.github.javafaker.Faker;
+
+public class Person {
+    private String fullName;
+
+    public Person(){
+        Faker faker = new Faker();
+        this.fullName = faker.name().fullName();
+    }
+}
