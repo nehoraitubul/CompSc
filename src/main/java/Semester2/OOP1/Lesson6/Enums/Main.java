068ae17c95dc9ffe3e344e9301d5106f6645ac57
nehoraitubul <nehorai0258@gmail.com>
2025-06-05 11:24:22 +0300

    REGULAR

---
+++ src/main/java/Semester2/OOP1/Lesson6/Enums/Main.java
@@ -0,0 +1,13 @@
+package Semester2.OOP1.Lesson6.Enums;
+
+public class Main {
+
+    public enum Day{
+        SUNDAY, MONDAY;
+    }
+
+    public static void main(String[] args) {
+        Day sunday = Day.SUNDAY;
+    }
+
+}
---
+++ src/main/java/Semester2/OOP1/Lesson6/Files/Main.java
@@ -0,0 +1,31 @@
+package Semester2.OOP1.Lesson6.Files;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try{
+            File file = new File("C:\\Users\\nehor\\OneDrive\\שולחן העבודה\\Try\\try1.txt");
+            if (file.exists()){
+                System.out.println("EXIST!");
+            } else {
+                file.createNewFile();
+                System.out.println("CREATED FILE!");
+            }
+//            FileReader fileReader = new FileReader(file);
+//            System.out.println((char) fileReader.read());
+            Scanner scanner = new Scanner(file);
+            System.out.println(scanner.nextLine());
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write("ניגה");
+
+            fileWriter.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
